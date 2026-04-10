a2fcd3e5f14d294de0a6f67f98cc21725feb508b
Chisom1Favour <favourchisom578@gmail.com>
2026-04-10 17:18:47 +0100

    New codes

---
+++ strings_and_projects/Halting_problem_timeout.py
@@ -0,0 +1,20 @@
+def safe_collector(count=2, max_attempts=10):
+    unique_names = {}
+    attempts = 0
+
+    while len(unique_names) < count and attempts < max_attempts:
+        name = input("Enter name: ").strip()
+        attempts += 1
+
+        if name.lower() in unique_names:
+            print(f"Duplicate! {max_attempts - attempts} attempts left")
+        else:
+            unique_names[name.lower()] = name
+
+    if len(unique_names) < count:
+        print("Maximum attemost reached. Exiting")
+
+    return list(unique_names.values())
+
+result = safe_collector()
+print(f"Names collected {result}")
